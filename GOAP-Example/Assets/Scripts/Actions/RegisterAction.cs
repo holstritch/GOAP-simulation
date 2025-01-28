23ad070792a72badaf008898546b090c4f3b1e25
holliep <hollie.page@livelystudio.com>
2025-01-28 14:42:25 +0000

    add register action to patient

---
+++ GOAP-Example/Assets/Scripts/Actions/RegisterAction.cs
@@ -0,0 +1,16 @@
+namespace Actions
+{
+    public class RegisterAction : GameAction
+    {
+        public override bool PrePerform()
+        {
+            return true;
+        }
+
+        public override bool PostPerform()
+        {
+            return true;
+        }
+    
+    }
+}
